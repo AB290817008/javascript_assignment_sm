90e04f9ead606d99c84d79f1f4ff82095d11df34
AB <42501497+AB290817008@users.noreply.github.com>
2023-05-08 18:39:41 +0500

    var...

---
+++ variable.js
@@ -0,0 +1,19 @@
+// var username;
+// var myName = "Sadia Abbasi";
+// var message;
+// message = "Hello World";
+// alert(message);
+// var studentName = "Abdul";
+// var age = "15 years old";
+// var course = "Certified Mobile Application Development";
+// alert(studentName);
+// alert(age);
+// alert(course);
+// var cars = ["PIZZA", "PIZZ", "PIZ", "PI", "P"];
+
+// let text = "";
+// for (let i = 0; i < cars.length; i++) {
+//   text += cars[i] + "\n";
+// }
+
+// alert(text);
