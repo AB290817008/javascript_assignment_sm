eb2c73b05cde4c7245633c0a628a8d32565b95df
AB <42501497+AB290817008@users.noreply.github.com>
2023-05-08 19:00:06 +0500

    var for string done

---
+++ variable_for_strings.js
@@ -0,0 +1,34 @@
+// var username;
+// var myName = "Sadia Abbasi";
+// var message;
+// message = "Hello World";
+// alert(message);
+// var studentName = "Abdul";
+// var age = "15 years old";
+// var course = "Certified Mobile Application Development";
+// alert(studentName);
+// alert(age);
+// alert(course);
+// var cars = ["PIZZA", "PIZZ", "PIZ", "PI", "P"];
+
+// let text = "";
+// for (let i = 0; i < cars.length; i++) {
+//   text += cars[i] + "\n";
+// }
+
+// alert(text);
+
+// var emailPrefix = "My email address is ";
+// var email = "xyz@gmail.com";
+// alert(emailPrefix.concat(email));
+
+// var bookPrefix = "I am trying to learn from the book ";
+// var book = "A smarter\n way to learn JavaScript";
+
+// alert(bookPrefix.concat(book));
+
+// document.write("Yah! I am writing HTML content through JavaScript");
+
+var symbols = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬";
+
+alert(symbols);
