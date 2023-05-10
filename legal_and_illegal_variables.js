2b26ee2a1fb101926b0b0c2bb2367f403088ada8
AB <42501497+AB290817008@users.noreply.github.com>
2023-05-10 14:37:24 +0500

    5 illegal vars done

---
+++ legal_and_illegal_variables.js
@@ -0,0 +1,23 @@
+//Declaring 3 variables in a single statement
+// var a,b,c;
+
+///Declaring 5 legal variables
+// var $name;
+// var _age;
+// var _fatherName;
+// var $motherFirstName;
+// var MobileNumber;
+
+
+///Declaring 5 illegal variables
+// var -name;
+// var &age;
+// var %fatherName;
+// var +motherFirstName;
+// var ^MobileNumber;
+
+//Display this in your browser
+// document.write("<strong>Rules for naming JS variables</strong>")
+// document.write("<strong>Rules for naming JS variables</strong>")
+
+
